8e87890b3fb2165b286ec30b47178a89428c3d8e
marc.thomas <marc.thomas@connect-group.com>
2016-05-01 21:14:54 +0100

    Added a create dialog for the items.

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,23 +1,65 @@
 const React = require('react');
 const client = require('./client');
 
+const follow = require('./follow'); // function to hop multiple links by "rel"
+
+const root = '/api';
+
 class App extends React.Component {
 
     constructor(props) {
         super(props);
-        this.state = {items: []};
+        this.onCreate = this.onCreate.bind(this);
+        this.state = {items: [], attributes: [], pageSize: 2, links: {}};
+    }
+
+    loadFromServer(pageSize) {
+        follow(client, root, [
+            {rel: 'items', params: {size: pageSize}}]
+        ).then(itemCollection => {
+            return client({
+                method: 'GET',
+                path: itemCollection.entity._links.profile.href,
+                headers: {'Accept': 'application/schema+json'}
+            }).then(schema => {
+                this.schema = schema.entity;
+                return itemCollection;
+            });
+        }).done(itemCollection => {
+            this.setState({
+                items: itemCollection.entity._embedded.items,
+                attributes: Object.keys(this.schema.properties),
+                pageSize: pageSize,
+                links: itemCollection.entity._links});
+        });
     }
 
     componentDidMount() {
-        client({method: 'GET', path: '/api/item'}).done(
-            response => {
-            this.setState({items: response.entity._embedded.item});
+        this.loadFromServer(this.state.pageSize);
+    }
+
+    onCreate(newItem) {
+        follow(client, root, ['items']).then(itemCollection => {
+            return client({
+                method: 'POST',
+                path: itemCollection.entity._links.self.href,
+                entity: newItem,
+                headers: {'Content-Type': 'application/json'}
+            })
+        }).then(response => {
+            return follow(client, root, [
+                {rel: 'items', params: {'size': this.state.pageSize}}]);
+        }).done(response => {
+            this.onNavigate(response.entity._links.last.href);
         });
     }
 
     render() {
         return (
-            <ItemList items={this.state.items}/>
+            <div>
+                <CreateDialog attributes={this.state.attributes} onCreate={this.onCreate}/>
+                <ItemList items={this.state.items}/>
+            </div>
         )
     }
 }
@@ -50,6 +92,59 @@ class Item extends React.Component {
             </tr>
         )
     }
+}
+
+class CreateDialog extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var newItem = {};
+        this.props.attributes.forEach(attribute => {
+            newItem[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
+        });
+        this.props.onCreate(newItem);
+
+        // clear out the dialog's inputs
+        this.props.attributes.forEach(attribute => {
+            React.findDOMNode(this.refs[attribute]).value = '';
+        });
+
+        // Navigate away from the dialog to hide it.
+        window.location = "#";
+    }
+
+    render() {
+        var inputs = this.props.attributes.map(attribute =>
+            <p key={attribute}>
+                <input type="text" placeholder={attribute} ref={attribute} className="field" />
+            </p>
+        );
+
+        return (
+            <div>
+                <a href="#createItem">Create</a>
+
+                <div id="createItem" className="modalDialog">
+                    <div>
+                        <a href="#" title="Close" className="close">X</a>
+
+                        <h2>Create new item</h2>
+
+                        <form>
+                            {inputs}
+                            <button onClick={this.handleSubmit}>Create</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
 }
 
 React.render(
