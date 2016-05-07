13f04a33b686e8550e0714c0478b57321ca19003
marc.thomas <marc.thomas@connect-group.com>
2016-05-07 21:21:40 +0100

    Created editable text field

---
+++ src/main/resources/static/EditableTextField.js
@@ -0,0 +1,46 @@
+import React from 'react';
+
+export default class EditableTextField extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: this.props.value, readOnly: this.props.readOnly};
+
+        this.handleSubmit = this.handleSubmit.bind(this);
+        this.handleTextChange = this.handleTextChange.bind(this);
+        this.componentWillReceiveProps = this.componentWillReceiveProps.bind(this);
+    }
+
+    handleSubmit(e) {
+        e.preventDefault();
+        var value = this.state.value;
+        this.props.handleFormSubmit(value);
+    }
+
+    handleTextChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    // Ensure the state of the component is updated before rendering
+    componentWillReceiveProps(nextProps) {
+        this.setState({value: nextProps.value, readOnly: nextProps.readOnly});
+    }
+
+    render() {
+        if (this.state.readOnly) {
+            return (
+                <div>{this.state.value}</div>
+            )
+        } else {
+            return (
+                <form onSubmit={this.handleSubmit}>
+                    <input type="text"
+                           placeholder="Your Value"
+                           value={this.state.value}
+                           onChange={this.handleTextChange}/>
+                    <input type="submit" value="Save" />
+                </form>
+            )
+        }
+
+    }
+}
