374b49697421c5ff8a055e99525fbd3d4ea0b3e2
marc.thomas <marc.thomas@connect-group.com>
2016-05-07 22:13:20 +0100

    Starting to add editable fields to item row

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -7,39 +7,11 @@ import ItemPage from './components/item/ItemPage';
 import RoomPage from './components/room/RoomPage';
 import StoragePage from './components/storage/StoragePage';
 
-import EditableTextField from './components/shared/EditableTextField';
-
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.onClickHandler = this.onClickHandler.bind(this);
-        this.onEditHandler = this.onEditHandler.bind(this);
-        this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.state = {value: 1, readOnly: true};
-    }
-
-    onClickHandler() {
-        this.setState({value: ++this.state.value});
-    }
-
-    onEditHandler() {
-        this.setState({readOnly: !this.state.readOnly});
-    }
-
-    handleFormSubmit(fieldValue) {
-        this.setState({value: fieldValue, readOnly: true});
-    }
-
     render() {
         return (
             <div>
                 <p>This system will allow you to find where anything should be stored in the church. Click on items to see the full list of all items.</p>
-
-                <input type="button" value="Increment" onClick={this.onClickHandler} />
-                <br/>
-                <input type="button" value="Toggle Edit" onClick={this.onEditHandler} />
-                <br/>
-                <EditableTextField value={this.state.value} readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit}/>
             </div>
         )
     }
--- src/main/resources/static/components/item/ItemRow.js
+++ src/main/resources/static/components/item/ItemRow.js
@@ -2,11 +2,29 @@ import React from 'react';
 import OrganisationControl from '../organisation/OrganisationControl';
 import StorageControl from '../storage/StorageControl';
 
+import EditableTextField from '../shared/EditableTextField';
+
 export default class ItemRow extends React.Component {
+    constructor(props) {
+        super(props);
+        this.onEditHandler = this.onEditHandler.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+        this.state = {value: this.props.item.name, readOnly: true};
+    }
+
+    onEditHandler() {
+        this.setState({readOnly: !this.state.readOnly});
+    }
+
+    handleFormSubmit(fieldValue) {
+        this.setState({value: fieldValue, readOnly: true});
+    }
+
     render() {
         return (
             <tr>
-                <td>{this.props.item.name}</td>
+                <td><input type="button" value={this.state.readOnly ? 'Edit' : 'Cancel'} onClick={this.onEditHandler} /></td>
+                <td><EditableTextField value={this.state.value} readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
                 <td>{this.props.item.description}</td>
                 <td>{this.props.item.notes}</td>
                 <td>{"" + this.props.item.isAsset}</td>
