2a90914dd252afbb602e8649e1895fac5560f994
marc.thomas <marc.thomas@connect-group.com>
2016-05-07 07:12:16 +0100

    Added other pages in

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -5,7 +5,9 @@ const client = require('./client');
 
 const follow = require('./follow'); // function to hop multiple links by "rel"
 const OrganisationPage = require('./components/organisation/OrganisationPage');
+const ItemPage = require('./components/item/ItemPage');
 const RoomPage = require('./components/room/RoomPage');
+const StoragePage = require('./components/storage/StoragePage');
 
 const CreateItemDialog = require('./components/CreateItemDialog');
 const ItemList = require('./components/ItemList');
@@ -74,8 +76,10 @@ class App extends React.Component {
 ReactDOM.render((
     <Router history={hashHistory}>
         <Route path="/" component={App}/>
+        <Route path="/item" component={ItemPage}/>
         <Route path="/room" component={RoomPage}/>
         <Route path="/organisation" component={OrganisationPage}/>
-        <Route path="/items" component={OrganisationPage}/>
+        <Route path="/items" component={ItemPage}/>
+        <Route path="/storage" component={StoragePage}/>
     </Router>
 ), document.getElementById('react'))
---
+++ src/main/resources/static/components/storage/StoragePage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default React.createClass({
+    render() {
+        return (
+            <div>
+                <h2>Storage</h2>
+            </div>)
+    }
+})
