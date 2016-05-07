c0b5519d1a37df73b86d049a4953e29b15eb46ef
marc.thomas <marc.thomas@connect-group.com>
2016-05-07 22:55:06 +0100

    Made all the text fields editable

--- src/main/resources/static/components/item/ItemRow.js
+++ src/main/resources/static/components/item/ItemRow.js
@@ -9,27 +9,36 @@ export default class ItemRow extends React.Component {
         super(props);
         this.onEditHandler = this.onEditHandler.bind(this);
         this.handleFormSubmit = this.handleFormSubmit.bind(this);
-        this.state = {value: this.props.item.name, readOnly: true};
+        this.state = {name: this.props.item.name,
+            description: this.props.item.description,
+            notes: this.props.item.notes,
+            isAsset: "" + this.props.item.isAsset,
+            value: "" + this.props.item.value,
+            reference: this.props.item.reference,
+            readOnly: true};
     }
 
     onEditHandler() {
         this.setState({readOnly: !this.state.readOnly});
     }
 
-    handleFormSubmit(fieldValue) {
-        this.setState({value: fieldValue, readOnly: true});
+    handleFormSubmit(fieldValue, fieldName) {
+        var stateObject = {};
+        stateObject[fieldName] = fieldValue;
+        stateObject['readOnly'] = true;
+        this.setState(stateObject);
     }
 
     render() {
         return (
             <tr>
                 <td><input type="button" value={this.state.readOnly ? 'Edit' : 'Cancel'} onClick={this.onEditHandler} /></td>
-                <td><EditableTextField value={this.state.value} readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
-                <td>{this.props.item.description}</td>
-                <td>{this.props.item.notes}</td>
-                <td>{"" + this.props.item.isAsset}</td>
-                <td>{"" + this.props.item.value}</td>
-                <td>{this.props.item.reference}</td>
+                <td><EditableTextField value={this.state.name} field='name' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
+                <td><EditableTextField value={this.state.description} field='description' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
+                <td><EditableTextField value={this.state.notes} field='notes' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
+                <td><EditableTextField value={this.state.isAsset} field='isAsset' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
+                <td><EditableTextField value={this.state.value} field='value' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
+                <td><EditableTextField value={this.state.reference} field='reference' readOnly={this.state.readOnly} handleFormSubmit={this.handleFormSubmit} /></td>
                 <td><StorageControl storage={this.props.item._links.storage.href}/></td>
                 <td><OrganisationControl organisation={this.props.item._links.organisation.href}/></td>
             </tr>
