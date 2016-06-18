13a6c1e47f8f058442e4672e6d6412c93557f94d
marc.thomas <marc.thomas@connect-group.com>
2016-06-18 22:16:05 +0100

    Adding the details for the create item dialog.

--- src/main/resources/static/components/item/CreateItemDialog.js
+++ src/main/resources/static/components/item/CreateItemDialog.js
@@ -4,47 +4,96 @@ export default class CreateItemDialog extends React.Component {
 
     constructor(props) {
         super(props);
-        this.handleSubmit = this.handleSubmit.bind(this);
     }
 
-    handleSubmit(e) {
-        e.preventDefault();
-        var newItem = {};
-        this.props.attributes.forEach(attribute => {
-            newItem[attribute] = React.findDOMNode(this.refs[attribute]).value.trim();
-        });
-        this.props.onCreate(newItem);
-
-        // clear out the dialog's inputs
-        this.props.attributes.forEach(attribute => {
-            React.findDOMNode(this.refs[attribute]).value = '';
-        });
-
-        // Navigate away from the dialog to hide it.
-        window.location = "#";
+    showModal() {
+        $('#createItem').modal();
     }
 
     render() {
-        var inputs = this.props.attributes.map(attribute =>
-            <p key={attribute}>
-                <input type="text" placeholder={attribute} ref={attribute} className="field" />
-            </p>
-        );
-
         return (
             <div>
-                <a href="#createItem">Create New Item</a>
+                <button className="btn btn-primary" onClick={this.showModal}>
+                    Create New Item
+                </button>
 
-                <div id="createItem" className="modalDialog">
-                    <div>
-                        <a href="#" title="Close" className="close">X</a>
+                <div id="createItem" className="modal fade">
+                    <div className="modal-dialog">
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h4 className="modal-title">Create a new item</h4>
+                                <button type="button" className="close" data-dismiss="modal" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                            </div>
 
-                        <h2>Create New Item</h2>
+                            <div className="modal-body">
+                                <form className="form-horizontal">
+                                    <div className="form-group">
+                                        <label for="name" className="col-sm-2 control-label">Name</label>
+                                        <div className="col-sm-10">
+                                            <input id="name"
+                                                className="form-control"
+                                                type="text"
+                                                placeholder="Please enter an item name" />
+                                        </div>
+                                    </div>
 
-                        <form>
-                            {inputs}
-                            <button onClick={this.handleSubmit}>Create</button>
-                        </form>
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label">Description</label>
+                                        <div className="col-sm-10">
+                                            <textarea
+                                                className="form-control"
+                                                type="textarea"
+                                                rows="3"
+                                                placeholder="Please enter an item description" />
+                                        </div>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label">Notes</label>
+                                        <div className="col-sm-10">
+                                            <textarea
+                                                className="form-control"
+                                                type="textarea"
+                                                rows="3"
+                                                placeholder="Please enter item notes" />
+                                        </div>
+                                    </div>
+
+                                    <div className="checkbox">
+                                        <label>
+                                            <input type="checkbox"> Is Asset </input>
+                                        </label>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label">Value</label>
+                                        <div className="col-sm-10">
+                                            <input
+                                                className="form-control"
+                                                type="number"
+                                                placeholder="Please enter item value" />
+                                        </div>
+                                    </div>
+
+                                    <div className="form-group">
+                                        <label className="col-sm-2 control-label">Reference</label>
+                                        <div className="col-sm-10">
+                                            <input
+                                                className="form-control"
+                                                type="text"
+                                                placeholder="Please enter item asset reference" />
+                                        </div>
+                                    </div>
+                                </form>
+                            </div>
+
+                            <div className="modal-footer">
+                                <button type="button" className="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="button" className="btn btn-primary">Save changes</button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
